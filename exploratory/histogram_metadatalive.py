ae532ead232fbc0021ac1e250fe864a1d9b674dc
ldocao <ldocao@gmail.com>
2015-08-04 22:09:47 +0100

    metadata.live figure, assuming live="direct retransmission"

--- exploratory/histogram_metadatalive.py
+++ exploratory/histogram_metadatalive.py
@@ -9,20 +9,22 @@ db = client.phoenix
 ##collections
 genre = db.genre
 event = db.event
-
+total_number_of_events = event.count()
 
 ##make the histogram
-fieldname = "locations.provider"
+fieldname = "metadata.live"
 data = count_occurences_field(event,fieldname)
 x = data.keys()
 y = data.values()
-X = np.arange(len(data))
 
-ymax = max(y)*1.2
+
+pos = np.arange(len(x))+0.5
 
 plt.figure()
 plt.barh(x, y)
+plt.axvline(total_number_of_events,linestyle="dashed",color="black")
 plt.xlabel("Counts")
-plt.ylabel("Provider")
-plt.savefig("figures/histogram_provider.pdf")
+plt.ylabel(fieldname)
+plt.yticks(pos,x)
+plt.savefig("figures/histogram_"+fieldname+".pdf")
 
