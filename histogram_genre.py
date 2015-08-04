e7a12c5652075e6a6dd01f53bb096ff88fc3a863
ldocao <ldocao@gmail.com>
2015-08-04 17:01:29 +0100

    final version

--- histogram_genre.py
+++ histogram_genre.py
@@ -1,3 +1,5 @@
+###PURPOSE: print the number of events which have a N number of events
+
 import pymongo
 
 client = pymongo.MongoClient("localhost", 27017) 
@@ -5,17 +7,12 @@ db = client.phoenix
 
 genre = db.genre
 event = db.event
-o_d_events = db.o_d_event
+
+maxn=10 #max number of genres we are looking for
+for n in range(0,maxn):
+    print event.find({ "topic.genres": { "$size": n } } ).count()
 
 
-for g in genre.find():
-    print g
 
-# ##make a list of all genre
-# list_genre=[]
-# for g in genre.find():
-#     try:
-#         list_genre.append(g["name"])
-#         i=i+1
-#     except(KeyError):
-#         print "no name"
+
+    
