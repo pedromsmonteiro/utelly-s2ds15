a2db381ad17851d077b32d1525bd2fcf8b542203
ldocao <ldocao@gmail.com>
2015-08-06 19:15:37 +0100

    add col names

--- clustering/transform/transform.py
+++ clustering/transform/transform.py
@@ -7,16 +7,37 @@ def call_field(collection,field_name,*args):
     number_of_extra_arguments = len(args)
     event_id = []
     field_value = []
-    
+
+    name_col1 = "_id"
+    name_col2 = field_name
     for i in collection.find():
-        event_id.append(i["_id"])
+        event_id.append(i[name_col1])
 
         if number_of_extra_arguments == 0:
             field_value.append(i[field_name])
         elif number_of_extra_arguments == 1:
             field_value.append(i[field_name][args[0]])
+            field_value+="."+args[0]
         elif number_of_extra_arguments == 2:
             field_value.append(i[field_name][args[0]][args[1]])
-    return [event_id,field_value]
+            field_value+="."+args[0]+"."+args[1]
+    return [event_id, field_value, name_col1, name_col2]
 
+
+
+def match_lists(list1,list2):
+    """Check if the event ID matches between list1 and list2
+
+    Parameters:
+    ----------
+    list1, list 2: list of 2 lists
+        output of call_field
+
+    Output:
+    ------
+    result : Boolean
+    """
+
+    result = list1[0]-list2[0]
+    return result
 
